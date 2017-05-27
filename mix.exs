22c4c0a68f1bd102232c28fc7f1dd36a9baf2768
Connor Jacobsen <jacobsen.connor@gmail.com>
2017-05-27 05:47:19 -0400

    tweaks to prep for hex.pm publishing

--- mix.exs
+++ mix.exs
@@ -1,13 +1,26 @@
 defmodule RedixPool.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "Simple Redis pooling built on redix and poolboy"
+  @github_url "https://github.com/opendoor-labs/redix_pool"
+
   def project do
     [app: :redix_pool,
-     version: "0.1.0",
+     version: @version,
+     description: @description,
+     package: package(),
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
+  end
+
+  def package do
+    [homepage_url: @github_url,
+     licenses: ["MIT"],
+     links: %{"Github" => @github_url},
+     source_url: @github_url]
   end
 
   # Configuration for the OTP application
@@ -15,10 +28,8 @@ defmodule RedixPool.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [
-      extra_applications: [:logger],
-      mod: {RedixPool, []}
-    ]
+    [extra_applications: [:logger],
+     mod: {RedixPool, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -31,9 +42,7 @@ defmodule RedixPool.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [
-      {:redix, "~> 0.6"},
-      {:poolboy, "~> 1.5"}
-    ]
+    [{:redix, "~> 0.6"},
+     {:poolboy, "~> 1.5"}]
   end
 end
